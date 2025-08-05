2dca32b6fb2796165b4e0d87af0836c276a6693a
Mudassir Khan <12mudassarkhan@gmail.com>
2025-08-05 22:14:16 +0530

    Built Basic middleware which logs the type of request, time of request and hostname.

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const methodOverRide = require('method-override');
+app.set("view engine","ejs");
+app.set("views", path.join(__dirname,"views"));
+app.use(methodOverRide("_method"));
+
+//middleware
+app.use((req,res,next)=>{
+    req.responseTime = new Date(Date.now()).toString();
+    console.log(req.method, req.path,req.responseTime,req.hostname);
+    next();
+});
+
+app.get("/",(req,res)=>{
+    res.render("home.ejs");
+});
+
+app.post("/req",(req,res)=>{
+    res.send("received");
+});
+
+//Server
+app.listen(8080,()=>{
+    console.log("Server running on Port: 8080");
+});
